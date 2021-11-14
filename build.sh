34de0ad446081853b52bb520a5eeaee1e7fb53ed
zztttt <1315691850@qq.com>
2021-11-14 10:28:35 +0800

    update shell

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+echo "pull redis:latest"
+docker pull redis:latest
+
+echo "pull mysql:5.7"
+docker pull mysql:5.7
+
 echo "build eureka"
 docker build -f eureka.Dockerfile -t eureka:latest .
 
