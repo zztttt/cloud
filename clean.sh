35c29b4efd48adc4a7e05bbeed692abf2804e8cf
zztttt <zztttt98@qq.com>
2021-11-16 21:21:53 +0800

    update shell

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+docker stop app1 app2 app3 eureka gateway
+docker rm app1 app2 app3 eureka gateway
--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
-echo "run redis"
-docker run -d -p 6380:6379 --name=redis redis:latest
-
-echo "run mysql-master"
-docker run -d -p 3300:3306 --name=mysql-master -e MYSQL_ROOT_PASSWORD=123456 mysql:5.7
-
-echo "run mysql-slave1"
-docker run -d -p 3301:3306 --name=mysql-slave1 -e MYSQL_ROOT_PASSWORD=123456 mysql:5.7
-
-echo "run mysql-slave2"
-docker run -d -p 3302:3306 --name=mysql-slave2 -e MYSQL_ROOT_PASSWORD=123456 mysql:5.7
-
-sleep 10s
+#echo "run redis"
+#docker run -d -p 6380:6379 --name=redis redis:latest
+#
+#echo "run mysql-master"
+#docker run -d -p 3300:3306 --name=mysql-master -e MYSQL_ROOT_PASSWORD=123456 mysql:5.7
+#
+#echo "run mysql-slave1"
+#docker run -d -p 3301:3306 --name=mysql-slave1 -e MYSQL_ROOT_PASSWORD=123456 mysql:5.7
+#
+#echo "run mysql-slave2"
+#docker run -d -p 3302:3306 --name=mysql-slave2 -e MYSQL_ROOT_PASSWORD=123456 mysql:5.7
+#
+#sleep 10s
 
 echo "run eureka"
 docker run --name=eureka -d -p 8761:8761 eureka:latest
